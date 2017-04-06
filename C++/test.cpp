37ffbb66b799e5a5b06db5169ae42eafd5cd7662
Chufan Deng <dengc@purdue.edu>
2017-04-06 16:33:13 -0700

    change c++ with iter

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,61 +1,42 @@
 #include<iostream>
 #include<vector>
 
-using namespace std ;
+using namespace std;
 
 vector<int> readVals();
-void printVals(vector<int> v);
 vector<int> valsGT0(vector<int> v);
 int findLast(vector<int> v, int target);
+void IterPrint(vector<int>v);
 
 int main()
 {
-    // vector<int> a(4,1) ;
-    //
-    // cin >>a[2] ;
-    //
-    // a.clear();
-    // a.insert(a.begin(), 1000);
-    // a.insert(a.begin()+1, 10);
-    // a.insert(a.begin()+2, 10);
-    // a.erase(a.begin());
-    // a[1] =99;
-
     vector<int> a;
     a = readVals();
-    cout << "Original:" << " ";
-    printVals(a);
+    cout << "Original: ";
+    IterPrint(a);
 
     vector<int> newA;
     newA = valsGT0(a);
-    cout << "Filtered:" << " ";
-    printVals(newA);
+    cout << "Filtered: ";
+    IterPrint(newA);
 
-    cout << findLast(a,23) << '\n';
+    cout << "result: " << findLast(a,23) << '\n';
     return 0 ;
 }
 
 vector<int> readVals(){
     int val;
     vector<int> v;
-    for (int i = 0; i < 5; i++) {
-        cin >> val;
+    while (cin >> val) {
         v.push_back(val);
     }
     return v;
 }
 
-void printVals(vector<int> v){
-    for(int i =0; i<v.size(); i++){
-        cout << v[i] << " ";
-    }
-    cout  << '\n';
-}
-
 vector<int> valsGT0(vector<int> v){
     vector<int> result;
-    for(int i=0; i<v.size(); i++){
-        if(v[i]>0){
+    for(int i = 0; i < v.size(); i++){
+        if(v[i] > 0){
             result.push_back(v[i]);
         }
     }
@@ -65,9 +46,17 @@ vector<int> valsGT0(vector<int> v){
 int findLast(vector<int> v, int target){
     int result = -1;
     for (int i = 0; i < v.size(); i++) {
-        if(v[i] == target && result < i){
+        if(v[i] == target){
             result = i;
         }
     }
     return result;
+}
+
+void IterPrint(vector<int>v){
+    vector<int>::iterator iter;
+    for (iter = v.begin(); iter != v.end(); iter++){
+        cout << (*iter) << " ";
+    }
+    cout  << '\n';
 }
